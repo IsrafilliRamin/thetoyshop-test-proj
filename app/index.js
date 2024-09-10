725c0174313dc60e89f54ed429e4e39ade824bb8
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2024-09-10 23:28:40 +0400

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,15 @@
+/* 
+$(function () {
+    $("div").click(function(){
+        $(this).css("color","red");
+    });
+    $("p").click(function(){});
+    $(".p").click(function(){});
+    $("#p").click(function(){});
+    $("#p .p").click(function(){});
+    $("[href]").click(function(){});
+    $("[id]").click(function(){});
+    $("[class]").click(function(){});
+  });
+ */
+
